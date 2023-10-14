13ffd8b303089a9750897d333e34bff1aed7cc54
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-10-14 09:41:35 +0500

    complete

--- ex44/Program.cs
+++ ex44/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ex44
 {
@@ -10,48 +7,126 @@ namespace ex44
     {
         static void Main(string[] args)
         {
+            const string CommandBuyWeapon = "1";
+            const string CommandShowProducts = "2";
+            const string CommandShowInventory = "3";
+            const string CommandExit = "4";
+
+            Customer customer = new Customer();
+            Player player = new Player();
+            bool isOpen = true;
+
+            while (isOpen)
+            {
+                Console.SetCursorPosition(40, 0);
+                player.ShowMoney();
+                Console.SetCursorPosition(0, 0);
+                Console.Write($"Оружейная\n\n" +
+                    $"{CommandBuyWeapon} - купить оружие\n" +
+                    $"{CommandShowProducts} - показать товары\n" +
+                    $"{CommandShowInventory} - показать инвентарь\n" +
+                    $"{CommandExit} - выйти из программы\n" +
+                    $"\nЧто желаете сделать? ");
+
+                switch (Console.ReadLine())
+                {
+                    case CommandBuyWeapon:
+                        player.MakeTransaction(customer);
+                        break;
+
+                    case CommandShowInventory:
+                        player.ShowInventory();
+                        break;
+
+                    case CommandShowProducts:
+                        customer.ShowProducts();
+                        break;
+
+                    case CommandExit:
+                        isOpen = false;
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
         }
     }
 
     class Player
     {
         private List<Product> _inventory = new List<Product>();
-        private int _money;
-        private int _moneyToPay;
 
         public Player()
         {
             Random random = new Random();
-            _money = random.Next(40, 101);
+            Money = random.Next(150, 501);
         }
 
-        public bool CheckSolnevcy(Product product)
-        {
-            _moneyToPay = product.Count * product.Cost;
+        public int Money { get; private set; }
+        public int MoneyToPay { get; private set; }
 
-            if (_money >= _moneyToPay)
+        public void ShowMoney()
+        {
+            Console.WriteLine($"Денег в кошельке: {Money} монет");
+        }
+
+        public void MakeTransaction(Customer customer)
+        {
+            Product product = customer.FindProduct();
+
+            if (product != null)
+            {
+                if (CheckSolnevcy(product))
+                {
+                    Pay();
+                    _inventory.Add(product);
+                    customer.SellProduct(product);
+                    Console.WriteLine("Товар успешно куплен...");
+                }
+                else
+                {
+                    Console.WriteLine("Не хватает монет...");
+                }
+            }
+        }
+
+        public void ShowInventory()
+        {
+            if (_inventory.Count > 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Инвентарь\n");
+
+                foreach (Product product in _inventory)
+                {
+                    Console.WriteLine($"|{product.Name}|");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Инвентарь пуст...");
+            }
+        }
+
+        private bool CheckSolnevcy(Product product)
+        {
+            MoneyToPay = product.Cost;
+
+            if (Money >= MoneyToPay)
             {
                 return true;
             }
             else
             {
-                _moneyToPay = 0;
+                MoneyToPay = 0;
                 return false;
             }
         }
 
-        public int Pay()
+        private void Pay()
         {
-            _money -= _moneyToPay;
-            return _moneyToPay;
-        }
-
-        public void GetProduct(Customer customer)
-        {
-            if (_money >= _moneyToPay)
-            {
-                _inventory.Add(customer.GiveProduct());
-            }
+            Money -= MoneyToPay;
         }
     }
 
@@ -61,62 +136,47 @@ namespace ex44
 
         public Customer()
         {
-            _products.Add(new Product("Капуста", 2, 10));
-            _products.Add(new Product("Картофель", 15, 5));
-            _products.Add(new Product("Свекла", 12, 2));
-            _products.Add(new Product("Лук", 9, 1));
+            _products.Add(new Product("Меч", 100));
+            _products.Add(new Product("Молот", 130));
+            _products.Add(new Product("Секира", 170));
+            _products.Add(new Product("Длинный лук", 150));
+            _products.Add(new Product("Кинжал", 40));
         }
 
         public void ShowProducts()
         {
+            int numberOfProduct = 1;
+
             foreach (Product product in _products)
             {
+                Console.Write($"{numberOfProduct}. ");
                 product.ShowInfo();
+                numberOfProduct++;
             }
         }
 
-        public Product GiveProduct()
+        public Product FindProduct()
         {
-            Console.Write("Какой товар вы хотите купить? ");
-
             if (TryGetProduct(out Product product))
-            {
-                Console.Write("Введите кол-во которое хотите купить: ");
-                int count = GetRightCount();
-
-                if (TryGetCount(product))
-                {
-                    product.Count -= count;
-                    return product;
-                }
-                else
-                {
-                    return null;
-                }
-            }
+                return product;
             else
-            {
                 return null;
-            }
         }
 
-        public int GetRightCount()
+        public void SellProduct(Product product)
         {
-            if (int.TryParse(Console.ReadLine(), out int count))
-            {
-                return count;
-            }
-            else
-            {
-                return 0;
-            }
+            _products.Remove(product);
         }
 
         private bool TryGetProduct(out Product product)
         {
+            ShowProducts();
+
+            Console.Write("Какой товар вы хотите купить? ");
+
             if (int.TryParse(Console.ReadLine(), out int productNumber))
             {
-                if (productNumber - 1 <= _products.Count && productNumber > 0)
+                if (productNumber - 1 < _products.Count && productNumber > 0)
                 {
                     product = _products[productNumber - 1];
                     return true;
@@ -135,46 +195,22 @@ namespace ex44
                 return false;
             }
         }
-
-        private bool TryGetCount(Product product)
-        {
-            if (int.TryParse(Console.ReadLine(), out int productCount))
-            {
-                if (productCount <= product.Count && productCount > 0)
-                {
-                    Console.WriteLine("Продукт успешно куплен...");
-                    return true;
-                }
-                else
-                {
-                    Console.WriteLine("Столько у меня нет...");
-                    return false;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Неккоректный ввод...");
-                return false;
-            }
-        }
     }
 
     class Product
     {
-        public Product(string name, int count, int cost)
+        public Product(string name, int cost)
         {
             Name = name;
-            Count = count;
             Cost = cost;
         }
 
         public string Name { get; private set; }
-        public int Count { get; private set; }
         public int Cost { get; private set; }
 
         public void ShowInfo()
         {
-            Console.WriteLine($"Товар: {Name}\nКоличество - {Count} шт.\nЦена: {Cost} монет\n");
+            Console.WriteLine($"{Name}\nЦена: {Cost} монет\n");
         }
     }
 }
